c17989403774636092f6dacd317dbbc641972a27
yizhakg <yizhakg.gmail.com>
2021-04-12 11:44:29 +0300

    update

--- server/DB/index.js
+++ server/DB/index.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose')
 require('dotenv').config()
 
-const dbConnection = process.env.DBCONNECTION
+const dbConnection = "mongodb+srv://yizhakg:Aa123456@cluster0.kcsau.mongodb.net/Books?retryWrites=true&w=majority"
 mongoose
     .connect(dbConnection, { useNewUrlParser: true , useUnifiedTopology: true},)
     .catch(err => {
