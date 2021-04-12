abe6e37336885ba9af8f9ec3b8c25666c442a1b5
yizhakg <yizhakg.gmail.com>
2021-04-12 15:15:51 +0300

    up

--- server/index.js
+++ server/index.js
@@ -16,20 +16,18 @@ db.on('error',()=>console.log("connection error"));
 
 // import routers
 const bookRouter = require('./routes/book-router')
-
-// ############################server upload settings#######################
-if (process.env.NODE_ENV === 'production') {
-    // Serve any static files
-    app.use(express.static(path.join(__dirname, '../client/build')));
-    // Handle React routing, return all requests to React app
-    app.get('*', (req, res)=>{
-        res.sendFile(path.join(__dirname, '../client/build', 'index.html'));
-    });
-}
-
-
 //app up and port setting
 app.listen(PORT, () => {
   console.log(chalk.bgGreen.bold(`MERN server is up with port:${PORT}✌ `));
 });
 app.use('/books',bookRouter)
+
+// ############################server upload settings#######################
+if (process.env.NODE_ENV === 'production') {
+  // Serve any static files
+  app.use(express.static(path.join(__dirname, '../client/build')));
+  // Handle React routing, return all requests to React app
+  app.get('*', (req, res)=>{
+      res.sendFile(path.join(__dirname, '../client/build', 'index.html'));
+  });
+}
