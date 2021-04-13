04cd43c3ad303d0cf9e44bc1396f6997f081ad4b
yizhakg <yizhakg.gmail.com>
2021-04-13 14:12:04 +0300

    up

--- server/index.js
+++ server/index.js
@@ -30,4 +30,7 @@ if (process.env.NODE_ENV === 'production') {
   app.get('*', (req, res)=>{
       res.sendFile(path.join(__dirname, '../client/build', 'index.html'));
   });
-  app.get('/', (req, res) => { res.send('Hello from Express!')
+}
+app.get('/', (req, res)=>{
+  res.send("hello world");
+});
