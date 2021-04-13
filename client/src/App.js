7978eb806e8f3bfd7aa72ff1e52e041bae71cb0c
yizhakg <yizhakg.gmail.com>
2021-04-13 14:07:13 +0300

    up

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,11 @@
 import React from 'react'
 import './App.css';
 import {Home} from './components/pages' 
+
+export function avg(...avg){
+return avg.reduce((sum,num)=>sum+num)/avg.length
+}
+
 function App() {
   return (
     <div className="App">
--- server/index.js
+++ server/index.js
@@ -30,4 +30,5 @@ if (process.env.NODE_ENV === 'production') {
   app.get('*', (req, res)=>{
       res.sendFile(path.join(__dirname, '../client/build', 'index.html'));
   });
+  app.get('/', (req, res) => { res.send('Hello from Express!')
 }
