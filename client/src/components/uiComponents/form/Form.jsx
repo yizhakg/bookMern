11392f3654e3cbf96f20bf61c97b672becb0f7d4
yizhakg <yizhakg.gmail.com>
2021-04-18 16:38:51 +0300

    fixed

--- client/src/components/uiComponents/form/Form.jsx
+++ client/src/components/uiComponents/form/Form.jsx
@@ -5,22 +5,16 @@ import axios from 'axios'
 export default function Form({ setRefresh }) {
   const addbook = (e) => {
     e.preventDefault();
-    const title = e.target[0].value;
-    const author = e.target[1].value;
-    const pages = e.target[2].value;
-    const year = e.target[3].value;
-    const country = e.target[4].value;
-    const language = e.target[5].value;
-    const link = e.target[6].value;
-    axios.post(`${process.env.REACT_APP_URL}add/`, {book:{
-      title:title,
-      author:author,
-      pages:pages,
-      year:year,
-      country:country,
-      language:language,
-      link:link,
-    }})
+    const book = {
+     title:e.target[0].value,
+     author:e.target[1].value,
+     pages:e.target[2].value,
+     year:e.target[3].value,
+     country:e.target[4].value,
+     language:e.target[5].value,
+     link:e.target[6].value,
+  }
+    axios.post(`${process.env.REACT_APP_URL}add/`, {book:book})
       .then((res) => {
         document.querySelector(".addConfirm").style.display="block"
         setTimeout(()=>{
@@ -38,33 +32,33 @@ export default function Form({ setRefresh }) {
       <form class="books-form" onSubmit={addbook}>
         <span className="input">
           <label htmlFor="title">Book Title</label>
-          <input id="title" type="text" />
+          <input id="title" type="text" required />
         </span>
         <span className="input">
           <label htmlFor="author">Book Author</label>
-          <input id="author" type="text" />
+          <input id="author" type="text" required />
         </span>
         <span className="input">
           <label htmlFor="pages">Book Pages</label>
-          <input id="pages" type="number" />
+          <input id="pages" type="number" required />
         </span>
         <span className="input">
           <label htmlFor="year">Book Year</label>
-          <input id="year" type="number" />
+          <input id="year" type="number" required />
         </span>
         <span className="input">
           <label htmlFor="country">Book Country</label>
-          <input id="country" type="text" />
+          <input id="country" type="text" required />
         </span>
         <span className="input">
           <label htmlFor="language">Book Language</label>
-          <input id="language" type="text" />
+          <input id="language" type="text" required />
         </span>
         <span className="input">
           <label htmlFor="link">Book Link</label>
-          <input id="link" type="text" />
+          <input id="link" type="text" required />
         </span>
-        <button type="submit">submit</button>
+        <button class="submitBtn" type="submit">Add Book</button>
         <p className="addConfirm">book added 👍</p>
       </form>
     </React.Fragment>
